3279c1a7b0658376b5d12ae7a76c4ddd013f14f0
K4YL3Y <kayleyprice20@gmail.com>
2021-11-08 18:33:58 +0000

    Update Shop.sql

--- Shop.sql
+++ Shop.sql
@@ -1,1 +1,29 @@
 CREATE TABLE department 
+(
+    DepartmentId    INT NOT NULL AUTO INCREMENT, 
+    Title           VARCHAR (100) NOT NULL,
+
+    PRIMARY KEY (DepartmentId)
+);
+
+CREATE TABLE product
+(
+    ProductId       INT NOT NULL AUTO INCREMENT, 
+    Title           VARCHAR (100) NOT NULL,
+    PriceInPence    INT NOT NULL,
+    SKU             CHAR (8) NOT NULL
+    DepartmentId    INT NOT NULL, 
+
+    PRIMARY KEY (ProductId),
+    FOREIGN KEY (DepartmentId) REFERENCES department (DeparmentId)
+);
+
+CREATE TABLE customer
+(
+    NameId          INT NOT NULL AUTO INCREMENT, 
+    Email           CHAR (8) NOT NULL
+    PhoneNumber     INT NOT NULL, 
+
+    PRIMARY KEY (CustomerId),
+    FOREIGN KEY (DepartmentId) REFERENCES department (DeparmentId)
+);
